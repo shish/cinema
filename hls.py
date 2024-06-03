067ddceb772fa708eaac6ff7394f1618dbbf48b9
Shish <shish@shishnet.org>
2024-06-03 09:44:54 +0100

    comments

--- hls.py
+++ hls.py
@@ -10,6 +10,7 @@ for fn_in in sys.argv[1:]:
     fn_srt = f"{fn_base}.srt"
     fn_vtt = f"{fn_base}.vtt"
 
+    # height, video bandwidth, audio bandwidth
     fmts = [
         (720, "3M", "256k"),
         (480, "2M", "192k"),
@@ -17,14 +18,18 @@ for fn_in in sys.argv[1:]:
     ]
 
     if os.path.exists(fn_in):
+        # generate VTT subtitles if needed
         if not os.path.exists(fn_vtt):
             if os.path.exists(fn_srt):
+                # convert .srt to .vtt
                 subprocess.run(["ffmpeg", "-i", fn_srt, fn_vtt])
             else:
+                # extract subtitles from the video container
                 subprocess.run(["ffmpeg", "-i", fn_in, fn_vtt])
 
         cmd = ["ffmpeg", "-i", fn_in]
 
+        # clone input video stream, resize clone #n to height=fmts[n][0]
         filt = "[0:v]split=%d" % len(fmts)
         filt += "".join(f"[v{d+1}]" for d in range(len(fmts))) + "; "
         filt += "; ".join([f"[v{n}]scale=w=-2:h={px}[v{n}out]" for n, (px, bw, abw) in enumerate(fmts, start=1)])
@@ -35,13 +40,28 @@ for fn_in in sys.argv[1:]:
         for n, (px, bw, abw) in enumerate(fmts, start=0):
             cmd.extend([
                 "-map", f"[v{n+1}out]",
-                f"-c:v:{n}", "libx264", "-profile:v", "high", "-pix_fmt", "yuv420p", "-x264-params", "nal-hrd=cbr:force-cfr=1",
-                f"-b:v:{n}", bw, f"-maxrate:v:{n}", bw, f"-minrate:v:{n}", bw, f"-bufsize:v:{n}", bw, "-preset", "slow", "-g", "48", "-sc_threshold", "0", "-keyint_min", "48"
+                # codec for video stream n
+                f"-c:v:{n}", "libx264",
+                # browsers support high 8-bit; need to specify this because otherwise
+                # ffmpeg will attempt to transcode a 10-bit source to 10-bit output
+                "-profile:v", "high", "-pix_fmt", "yuv420p",
+                # ???
+                "-x264-params", "nal-hrd=cbr:force-cfr=1",
+                # bitrate for video stream n
+                f"-b:v:{n}", bw, f"-maxrate:v:{n}", bw, f"-minrate:v:{n}", bw, f"-bufsize:v:{n}", bw,
+                # ???
+                "-preset", "slow", "-g", "48", "-sc_threshold", "0", "-keyint_min", "48"
             ])
 
         for n, (px, bw, abw) in enumerate(fmts, start=0):
             cmd.extend([
-                "-map", f"a:0", f"-c:a:{n}", "aac", f"-b:a:{n}", abw, "-ac", "2"
+                "-map", f"a:0",
+                # codec for audio stream n
+                f"-c:a:{n}", "aac",
+                # bitrate for audio stream n
+                f"-b:a:{n}", abw,
+                # stereo
+                "-ac", "2"
             ])
 
         cmd.extend([
