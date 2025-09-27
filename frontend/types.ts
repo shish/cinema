30c0bfcf4bf4363ef27cb2b39d874b0e01f3cffe
Shish <shish@shishnet.org>
2025-09-27 05:51:04 +0100

    [fe] give up on .d.ts file

--- frontend/types.ts
+++ frontend/types.ts
@@ -1,10 +1,3 @@
-/// <reference types="@rsbuild/core/types" />
-
-declare module '*.svg?react' {
-    const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
-    export default ReactComponent;
-}
-
 type PlayingState = {
     paused?: number;
     playing?: number;
