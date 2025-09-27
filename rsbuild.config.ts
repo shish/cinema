5bf97ae69465b09cdb353cd3836299e6262175a0
Shish <shish@shishnet.org>
2025-09-27 05:39:56 +0100

    [fe] Set viewport option to resizes-content

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -1,26 +1,25 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
-import { pluginSass } from '@rsbuild/plugin-sass';
-import { pluginSvgr } from '@rsbuild/plugin-svgr';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginSass } from "@rsbuild/plugin-sass";
 
-const backend = process.env.BACKEND ?? 'http://127.0.0.1:8074';
+const backend = process.env.BACKEND ?? "http://127.0.0.1:8074";
 
 export default defineConfig({
-    plugins: [pluginReact(), pluginSass(), pluginSvgr({ svgrOptions: { svgProps: { width: '1em', height: '1em' } } })],
+    plugins: [pluginReact(), pluginSass()],
     source: {
-        entry: { index: './frontend/index.tsx' },
+        entry: { index: "./frontend/index.tsx" },
     },
     html: {
-        title: 'Cinema',
-        favicon: 'frontend/static/favicon.svg',
-        // template: 'frontend/static/index.html',
+        title: "Cinema",
+        favicon: "frontend/static/favicon.svg",
+        template: "frontend/static/index.html",
     },
     server: {
         proxy: {
-            '/movies': { target: backend },
-            '/rooms': { target: backend },
-            '/time': { target: backend },
-            '/room': { target: backend, ws: true },
+            "/movies": { target: backend },
+            "/rooms": { target: backend },
+            "/time": { target: backend },
+            "/room": { target: backend, ws: true },
         },
     },
 });
