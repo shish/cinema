369f34ceaac2d4a57ee1f5853f284de80e8e2e97
Shish <shish@shishnet.org>
2026-01-26 03:15:48 +0000

    remove unused loading bool

--- cinema/frontend/providers/server.tsx
+++ cinema/frontend/providers/server.tsx
@@ -3,14 +3,12 @@ import type { Movie } from '../types';
 
 export type ServerContextType = {
     movies: { [key: string]: Movie };
-    loading: boolean;
 };
 
 export const ServerContext = createContext<ServerContextType>({} as ServerContextType);
 
 export function ServerProvider({ children }: { children: React.ReactNode }) {
     const [movies, setMovies] = useState<{ [name: string]: Movie }>({});
-    const loading = Object.keys(movies).length === 0;
 
     useEffect(() => {
         fetch(`/files/movies.json?_=${Date.now()}`)
@@ -21,25 +19,10 @@ export function ServerProvider({ children }: { children: React.ReactNode }) {
             });
     }, []);
 
-    /*
-    const refreshMovies = useCallback(() => {
-        setMovies([]);
-        fetch('/movies')
-            .then((response) => response.json())
-            .then((movies) => {
-                setMovies(movies);
-                // console.log('Movies:', movies);
-            })
-            .catch((error) => {
-                console.error('Error loading movies:', error);
-            });
-    }, []);
-*/
     return (
         <ServerContext.Provider
             value={{
                 movies,
-                loading,
             }}
         >
             {children}
